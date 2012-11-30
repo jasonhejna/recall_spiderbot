3328c6f61c042db30eab607fc4363715d4506c06
Jason Hejna <jason.hejna@gmail.com>
2012-11-30 00:27:24 -0500

    three part junk working

--- fda.php
+++ fda.php
@@ -5,14 +5,14 @@ require 'connectz.php';
 //$alala = 'http://www.fda.gov/Safety/Recalls/default.htm';
 $file = file_get_contents('http://www.fda.gov/AboutFDA/ContactFDA/StayInformed/RSSFeeds/Recalls/rss.xml');
 //echo $file;
-preg_match_all('~<pubDate>(.*?)</pubDate>~',$file,$datedata);
+//preg_match_all('~<pubDate>(.*?)</pubDate>~',$file,$datedata);
 preg_match_all('~<link>(.*?)</link>~',$file,$urldata);
 //print_r($urldata);
-foreach ($datedata as $key => $value) {
+/*foreach ($datedata as $key => $value) {
 	foreach ($value as $key => $value) {
 		//echo $value . '<br>';
 	}
-}
+}*/
 foreach ($urldata as $key => $value) {
 	foreach ($value as $key => $value) {
 		//echo $value . '<br>';
@@ -23,9 +23,98 @@ foreach ($urldata as $key => $value) {
 		}
 		//echo $gogo . '<br>';
 		$onefile = file_get_contents($gogo);
+//get the name and date here once, since we are in the foreach loop and $onefile in our content
 
+// also, do all the preg matches at the same time, then itterate through one function of the foreach loop
 
-		preg_match_all('~[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}~',$onefile,$onedata);
+		//$matcherone = '~[0-9]{1}';
+		$m1 = '[0-9]{1}';
+		$m2 = '[0-9]{1}[0-9]{1}';
+		$m3 = '[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m4 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m5 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m6 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m7 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m8 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m9 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m10 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m11 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m12 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m13 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		$m14 = '[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}';
+		while ($g <= 14)
+		$g=0;
+		while ($g <= 13) {
+			$digittwo = 14 - $g;
+			$g++;
+			$concattwo = 'm' . $digittwo;
+			$concattwor = 'm' . $g;
+			$concat = "'~" . $$concattwo . "." .$$concattwor . "~'";
+			echo $concat . '<br>';
+		}
+		$g=0;
+		while ($g <= 12) {
+			$digittwo = 13 - $g;
+			$g++;
+			$concattwo = 'm' . $digittwo;
+			$concattwor = 'm' . $g;
+			$concat = "'~" . $$concattwo . "." .$$concattwor . "~'";
+			echo $concat . '<br>';
+		}
+		$g=0;
+		while ($g <= 11) {
+			$digittwo = 12 - $g;
+			$g++;
+			$concattwo = 'm' . $digittwo;
+			$concattwor = 'm' . $g;
+			$concat = "'~" . $$concattwo . "." .$$concattwor . "~'";
+			echo $concat . '<br>';
+		}
+		$g=0;
+		while ($g <= 10) {
+			$digittwo = 11 - $g;
+			$g++;
+			$concattwo = 'm' . $digittwo;
+			$concattwor = 'm' . $g;
+			$concat = "'~" . $$concattwo . "." .$$concattwor . "~'";
+			echo $concat . '<br>';
+		}
+		$g=0;
+		while ($g <= 9) {
+			$digittwo = 10 - $g;
+			$g++;
+			$concattwo = 'm' . $digittwo;
+			$concattwor = 'm' . $g;
+			$concat = "'~" . $$concattwo . "." .$$concattwor . "~'";
+			echo $concat . '<br>';
+		}
+		$g=0;
+		while ($g <= 8) {
+			$digittwo = 9 - $g;
+			$g++;
+			$concattwo = 'm' . $digittwo;
+			$concattwor = 'm' . $g;
+			$concat = "'~" . $$concattwo . "." .$$concattwor . "~'";
+			echo $concat . '<br>';
+		}
+		//start of the two character breaks
+		$g=0;
+		$o=0;
+		while ($g <= 7) {
+			$digittwo = 3 - $g;
+			$digitone = 3 + $g;
+			$o++;
+			$g++;
+			$concattwo = 'm' . $digittwo;
+			$concattwor = 'm' . $g;
+			$concat = "'~" . $$concattwo . "." .$$concattwor . "." .$$concatthree "~'";
+			$concattwo = "'~" . $$concattwo . "." .$$concattwor . "." .$$concatthree "~'";
+			echo $concat . '<br>' . $concattwo . '<br>';
+		}
+		//$matchtimes = $matchtimes * 2;
+		echo '<br>';
+		/*
+		preg_match_all($matcher,$onefile,$onedata);
 		foreach ($onedata as $key => $value) {
 			foreach ($value as $key => $value) {
 				$i=0;
@@ -46,7 +135,8 @@ foreach ($urldata as $key => $value) {
 				}
 		} //end foreach
 		}
-
+		*/
+/*
 		preg_match_all('~[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}[0-9]{1}~',$onefile,$oneadata);
 		foreach ($oneadata as $key => $value) {
 			foreach ($value as $key => $value) {
@@ -261,7 +351,7 @@ foreach ($urldata as $key => $value) {
 				}
 		} //end foreach
 		}
-
+*/
 	}
 }
 
